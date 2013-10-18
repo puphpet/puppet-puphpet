f1424b155a780ddac6dd771d1eabc74ab03a39cb
Juan Treminio <jtreminio@gmail.com>
2013-10-18 08:52:46 -0500

    Slimming down to only require elements

--- manifests/ini.pp
+++ manifests/ini.pp
@@ -1,27 +1,192 @@
-# defined type puphpet::ini
+# Defines where we can expect PHP ini files and paths to be located.
 #
-# Writes custom ini files. This generation
-# is independent from any other resource, so you have to
-# notify the needed resources on your own.
+# Different OS, OS version, webserver and PHP versions all contributes
+# to making a mess of where we can expect INI files to be found.
 #
-# Example:
-# puphpet::ini { 'custom_php':
-#  value   => ['date.timezone = "America/Chicago"'],
-#  ini     => '/etc/php5/cgi/conf.d/custom_php.ini',
-#  notify  => Service['apache'],
-# }
+# I have listed a bunch of places:
+#
+# 5.3
+#     DEBIAN 6 - squeeze
+#         APACHE
+#             FOLDERS: apache2/  cli/  conf.d/  php.ini
+#             /etc/php5/apache2/conf.d    -> /etc/php5/conf.d
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d
+#         NGINX
+#             FOLDERS: cli/  conf.d/  fpm/
+#             /etc/php5/fpm/conf.d        -> /etc/php5/conf.d
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d
+#     DEBIAN 7 - wheezy
+#         APACHE
+#             NOT CORRECT; PHP 5.4 INSTALLED
+#         NGINX
+#             NOT CORRECT; PHP 5.4 INSTALLED
+#     UBUNTU 10.04 - lucid
+#         APACHE
+#             FOLDERS: apache2/  cli/  conf.d/  php.ini
+#             /etc/php5/apache2/conf.d    -> /etc/php5/conf.d
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d
+#         NGINX
+#             FOLDERS: cli/  conf.d/  fpm/
+#             /etc/php5/fpm/conf.d        -> /etc/php5/conf.d
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d
+#     UBUNTU 12.04 - precise
+#         APACHE
+#             FOLDERS: apache2/  cli/  conf.d/  php.ini
+#             /etc/php5/apache2/conf.d    -> /etc/php5/conf.d
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d
+#         NGINX
+#             FOLDERS: cli/  conf.d/  fpm/
+#             /etc/php5/fpm/conf.d        -> /etc/php5/conf.d
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d
+# 5.4
+#     DEBIAN 6 - squeeze
+#         APACHE
+#             FOLDERS: apache2/  cli/  conf.d/  mods-available/  php.ini
+#             /etc/php5/apache2/conf.d    -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#         NGINX
+#             FOLDERS: cli/  conf.d/  fpm/  mods-available/
+#             /etc/php5/fpm/conf.d/*      -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d/*      -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#     DEBIAN 7 - wheezy
+#         APACHE
+#             FOLDERS: apache2/  cli/  conf.d/  mods-available/  php.ini
+#             /etc/php5/apache2/conf.d    -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#         NGINX
+#             FOLDERS: cli/  conf.d/  fpm/  mods-available/
+#             /etc/php5/fpm/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#     UBUNTU 10.04 - lucid
+#         APACHE
+#             FOLDERS: apache2/  cli/  conf.d/  mods-available/  php.ini
+#             /etc/php5/apache2/conf.d    -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#         NGINX
+#             FOLDERS: cli/  conf.d/  fpm/  mods-available/
+#             /etc/php5/fpm/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#     UBUNTU 12.04 - precise
+#         APACHE
+#             FOLDERS: apache2/  cli/  conf.d/  mods-available/  php.ini
+#             /etc/php5/apache2/conf.d    -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#         NGINX
+#             FOLDERS: cli/  conf.d/  fpm/  mods-available/
+#             /etc/php5/fpm/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d        -> /etc/php5/conf.d/*   -> /etc/php5/mods-available/*
+# 5.5
+#     DEBIAN 6 - squeeze
+#         APACHE
+#             NOT A VALID OPTION
+#         NGINX
+#             NOT A VALID OPTION
+#     DEBIAN 7 - wheezy
+#         APACHE
+#             FOLDERS: apache2/  cli/  mods-available/  php.ini
+#             /etc/php5/apache2/conf.d/*  -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d/*      -> /etc/php5/mods-available/*
+#         NGINX
+#             FOLDERS: cli/  fpm/  mods-available/
+#             /etc/php5/fpm/conf.d/*      -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d/*      -> /etc/php5/mods-available/*
+#     UBUNTU 10.04 - lucid
+#         APACHE
+#             NOT A VALID OPTION
+#         NGINX
+#             NOT A VALID OPTION
+#     UBUNTU 12.04 - precise
+#         APACHE
+#             NOT A VALID OPTION
+#         NGINX
+#             FOLDERS: cli/  fpm/  mods-available/
+#             /etc/php5/fpm/conf.d/*      -> /etc/php5/mods-available/*
+#             /etc/php5/cli/conf.d/*      -> /etc/php5/mods-available/*
+#
+# This depends on example42/puppet-php: https://github.com/example42/puppet-php
 #
 define puphpet::ini (
-    $value    = '',
-    $ini      = undef,
-    $template = 'extra-ini.erb'
-) {
+  $php_version,
+  $webserver,
+  $ini_filename = 'custom.ini',
+  $entry,
+  $value  = '',
+  $ensure = present
+  ) {
 
-  if $ini {
-      file { $ini:
-        ensure  => present,
-        content => template("puphpet/${template}"),
+  $real_webserver = $webserver ? {
+      'apache'          => 'apache2',
+      'httpd'           => 'apache2',
+      'apache2'         => 'apache2',
+      'nginx'           => 'fpm',
+      'fpm'             => 'fpm',
+  }
+
+  case $php_version {
+    '5.3', '53': {
+      case $::osfamily {
+        'debian': {
+          $target_dir  = '/etc/php5/conf.d'
+          $target_file = "${target_dir}/${ini_filename}"
+
+          ! defined(File[$target_file]) {
+            file { $target_file:
+              replace => no,
+              ensure  => present,
+            }
+          }
+
+          php::augeas{ "${entry}-${value}" :
+            target  => $target_file,
+            entry   => $entry,
+            value   => $value,
+            ensure  => $ensure,
+            require => File[$target_file]
+          }
+
+          $webserver_ini_location = $real_webserver ? {
+              'apache' => '/etc/php5/apache2/conf.d',
+              'fpm'    => '/etc/php5/fpm/conf.d',
+          }
+
+          $cli_ini_location = '/etc/php5/cli/conf.d'
+
+          ! defined(File["${webserver_ini_location}/${ini_filename}"]) {
+            file { "${webserver_ini_location}/${ini_filename}":
+              ensure  => link,
+              target  => $target_file,
+              require => Php::Augeas["${entry}-${value}"],
+            }
+          }
+
+          ! defined(File["${cli_ini_location}/${ini_filename}"]) {
+            file { "${cli_ini_location}/${ini_filename}":
+              ensure  => link,
+              target  => $target_file,
+              require => Php::Augeas["${entry}-${value}"],
+            }
+          }
+        }
       }
+      default: { fail('This OS has not yet been defined!') }
+    }
+    '5.4', '54': {
+      case $::osfamily {
+        'debian': {
+          #
+        }
+      }
+      default: { fail('This OS has not yet been defined!') }
+    }
+    '5.5', '55': {
+      case $::osfamily {
+        'debian': {
+          #
+        }
+      }
+      default: { fail('This OS has not yet been defined!') }
+    }
+    default: { fail('Unrecognized PHP version') }
   }
 
 }
