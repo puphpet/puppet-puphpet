ecc237444f9e237f8e2276428c6ff1ce71ee38cb
Juan Treminio <jtreminio@gmail.com>
2013-11-06 08:59:50 -0600

    Support for overwriting the default.conf nginx file

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,17 @@ class puphpet::params {
 
   $xdebug_package = $::osfamily ? {
     'Debian' => 'php5-xdebug',
-    'Redhat' => 'php-pecl-xdebug',
+    'Redhat' => 'php-pecl-xdebug'
+  }
+
+  $apache_webroot_location = $::osfamily ? {
+    'Debian' => '/var/www',
+    'Redhat' => '/var/www/html'
+  }
+
+  $nginx_webroot_location = $::osfamily ? {
+    'Debian' => '/var/www/html',
+    'Redhat' => '/usr/share/nginx/html'
   }
 
 }
