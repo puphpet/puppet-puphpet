1169b21675a6697e232565633507f33fd42ced72
Juan Treminio <jtreminio@gmail.com>
2014-01-06 01:30:23 -0600

    Adds adminer class

---
+++ manifests/adminer.pp
@@ -0,0 +1,20 @@
+class puphpet::adminer(
+  $location,
+  $owner = 'www-data'
+) {
+
+  if ! defined(File[$location]) {
+    file { $location:
+      replace => no,
+      ensure  => directory,
+      mode    => 775,
+      require => Package['php']
+    }
+  }
+
+  exec{ "download adminer to ${location}":
+    command => "wget -O ${location}/index.php http://www.adminer.org/latest.php",
+    require => File[$location],
+  }
+
+}
