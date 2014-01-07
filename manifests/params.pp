1fe0b774b4e497c49a83ff80ad0aeb89e27ecc44
Juan Treminio <jtreminio@gmail.com>
2014-01-06 23:00:58 -0600

    Added mod-pagespeed

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,6 +10,19 @@ class puphpet::params {
     'Redhat' => '/var/www/html'
   }
 
+  $apache_mod_pagespeed_url = $::osfamily ? {
+    'Debian' => $::architecture ? {
+        'i386'  => 'https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_i386.deb',
+        'amd64' => 'https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_amd64.deb'
+      },
+    'Redhat' => $::architecture ? {
+        'i386'  => 'https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_i386.rpm',
+        'amd64' => 'https://dl-ssl.google.com/dl/linux/direct/mod-pagespeed-stable_current_x86_64.rpm'
+      },
+  }
+
+  $apache_mod_pagespeed_package = 'mod-pagespeed-stable'
+
   $nginx_webroot_location = $::osfamily ? {
     'Debian' => '/var/www/html',
     'Redhat' => '/usr/share/nginx/html'
