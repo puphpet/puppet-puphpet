65f9346d42fea315e17cf19dfcc00bae2e86dc3f
Juan Treminio <jtreminio@gmail.com>
2014-05-25 01:49:59 -0500

    Creates snakeoil SSL certs

--- manifests/params.pp
+++ manifests/params.pp
@@ -74,4 +74,14 @@ class puphpet::params {
     'Redhat' => 'hhvm'
   }
 
+  $ssl_cert_location = $::osfamily ? {
+    'Debian' => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
+    'Redhat' => '/etc/ssl/certs/ssl-cert-snakeoil'
+  }
+
+  $ssl_key_location = $::osfamily ? {
+    'Debian' => '/etc/ssl/private/ssl-cert-snakeoil.key',
+    'Redhat' => '/etc/ssl/certs/ssl-cert-snakeoil'
+  }
+
 }
