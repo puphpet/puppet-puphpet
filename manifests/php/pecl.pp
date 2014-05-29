8808100433927c4d317c67b10e802ba1686332bb
Juan Treminio <jtreminio@gmail.com>
2014-05-29 12:37:45 -0500

    Adds translation for ZendOpCache. Fixes https://github.com/puphpet/puphpet/issues/477

--- manifests/php/pecl.pp
+++ manifests/php/pecl.pp
@@ -18,33 +18,53 @@ define puphpet::php::pecl (
     }
   }
 
-  $package = $::osfamily ? {
+  $pecl_beta = $::osfamily ? {
     'Debian' => {
-      'apc'       => $::operatingsystem ? {
-        'ubuntu' => 'php5-apcu',
-        'debian' => 'php5-apc',
+      'zendopcache' => $::operatingsystem ? {
+        'debian' => false,
+        'ubuntu' => 'ZendOpcache',
       },
-      'apcu'      => 'php5-apcu',
-      'imagick'   => 'php5-imagick',
-      'memcache'  => 'php5-memcache',
-      'memcached' => 'php5-memcached',
-      'mongo'     => 'php5-mongo',
     },
     'Redhat' => {
-      'apc'       => 'php-pecl-apcu',
-      'apcu'      => 'php-pecl-apcu',
-      'imagick'   => 'php-pecl-imagick',
-      'memcache'  => 'php-pecl-memcache',
-      'memcached' => 'php-pecl-memcached',
-      'mongo'     => 'php-pecl-mongo',
+      #
+    }
+  }
+
+  $package = $::osfamily ? {
+    'Debian' => {
+      'apc'         => $::operatingsystem ? {
+        'debian' => 'php5-apc',
+        'ubuntu' => 'php5-apcu',
+      },
+      'apcu'        => 'php5-apcu',
+      'imagick'     => 'php5-imagick',
+      'memcache'    => 'php5-memcache',
+      'memcached'   => 'php5-memcached',
+      'mongo'       => 'php5-mongo',
+      'zendopcache' => 'php5-zendopcache',
+    },
+    'Redhat' => {
+      'apc'         => 'php-pecl-apcu',
+      'apcu'        => 'php-pecl-apcu',
+      'imagick'     => 'php-pecl-imagick',
+      'memcache'    => 'php-pecl-memcache',
+      'memcached'   => 'php-pecl-memcached',
+      'mongo'       => 'php-pecl-mongo',
+      'zendopcache' => 'php-pecl-zendopcache',
     }
   }
 
   $downcase_name = downcase($name)
 
   if has_key($pecl, $downcase_name) {
-    $pecl_name    = $pecl[$downcase_name]
-    $package_name = false
+    $pecl_name       = $pecl[$downcase_name]
+    $package_name    = false
+    $preferred_state = 'stable'
+  }
+  elsif has_key($pecl_beta, $downcase_name) and $pecl_beta[$downcase_name] {
+    $pecl_name       = $pecl_beta[$downcase_name]
+    $package_name    = false
+    $preferred_state = 'beta'
   }
   elsif has_key($package, $downcase_name) {
     $pecl_name    = false
@@ -58,6 +78,7 @@ define puphpet::php::pecl (
   if $pecl_name and ! defined(Php::Pecl::Module[$pecl_name]) {
     php::pecl::module { $pecl_name:
       use_package         => false,
+      preferred_state     => $preferred_state,
       service_autorestart => $service_autorestart,
     }
   }
