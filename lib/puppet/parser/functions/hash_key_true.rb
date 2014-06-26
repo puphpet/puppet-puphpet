012d413bf432c4125740becd0f61ced295f6b175
Juan Treminio <jtreminio@gmail.com>
2014-06-26 10:20:52 -0500

    Thanks to @jcarouth for pointing out that this actually checks against a hash, not an array

--- lib/puppet/parser/functions/hash_key_true.rb
+++ lib/puppet/parser/functions/hash_key_true.rb
@@ -1,22 +1,22 @@
 #
-# array_key_true.rb
+# hash_key_true.rb
 #
 
 module Puppet::Parser::Functions
 
-  newfunction(:array_key_true, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
+  newfunction(:hash_key_true, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
 
-    Returns true if the key within array is truthy
+    Returns true if the key within hash is truthy
     ENDHEREDOC
 
     unless args.length == 2
-      raise Puppet::ParseError, ("array_key_true(): wrong number of arguments (#{args.length}; must be 2)")
+      raise Puppet::ParseError, ("hash_key_true(): wrong number of arguments (#{args.length}; must be 2)")
     end
 
     arr = args[0]
     key = args[1]
 
-    unless arr.is_a?(Array)
+    unless arr.is_a?(Hash)
       return false
     end
 
