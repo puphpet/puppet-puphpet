09dfede239205b6bd72b035d506176992c6e5ad6
Juan Treminio <jtreminio@gmail.com>
2014-06-27 20:35:46 -0500

    Adds value_true function

---
+++ lib/puppet/parser/functions/value_true.rb
@@ -0,0 +1,33 @@
+#
+# value_true.rb
+#
+
+module Puppet::Parser::Functions
+
+  newfunction(:value_true, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
+
+    Returns true if the key within hash is truthy
+    ENDHEREDOC
+
+    unless args.length == 1
+      raise Puppet::ParseError, ("value_true(): wrong number of arguments (#{args.length}; must be 1)")
+    end
+
+    value = args[0]
+
+    if value.nil?
+      return false
+    end
+
+    if value.empty?
+      return false
+    end
+
+    if value == 'false'
+      return false
+    end
+
+    return true
+
+  end
+end
