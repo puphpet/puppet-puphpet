25b0158a9902bf0dc4f3883077e599d493debfe1
Juan Treminio <jtreminio@gmail.com>
2015-03-02 14:58:37 -0600

    Adds another depends comment for puppet-composer

--- manifests/beanstalkd/console.pp
+++ manifests/beanstalkd/console.pp
@@ -1,5 +1,6 @@
 # This depends on puppetlabs-vcsrepo: https://github.com/puppetlabs/puppetlabs-vcsrepo.git
-# This depends on puppet/beanstalkd: https://github.com/puphpet/puppet-beanstalkd
+# This depends on puppet-beanstalkd: https://github.com/puphpet/puppet-beanstalkd
+# This depends on puppet-composer: https://github.com/tPl0ch/puppet-composer
 # Installs beanstalk_console from https://github.com/ptrofimov/beanstalk_console
 class puphpet::beanstalkd::console(
   $install_location
