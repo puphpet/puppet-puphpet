e7de40a9e11652f63c778e0548fbb92e4f0beec6
Juan Treminio <jtreminio@gmail.com>
2015-03-02 16:28:58 -0600

    Fixes code to actually work!

--- manifests/firewall/port.pp
+++ manifests/firewall/port.pp
@@ -1,9 +1,13 @@
 # This depends on puppetlabs/firewall: https://github.com/puppetlabs/puppetlabs-firewall
 # Adds a firewall rule
 define puphpet::firewall::port (
-  $host,
-  $guest,
+  $port,
+  $protocol = tcp,
+  $action   = 'accept',
+  $priority = 100,
 ) {
+  $rule_name = "${rule['priority']} ${rule['proto']}/${rule['port']}"
+
   if ! defined(Firewall["100 tcp/${guest}"]) {
     firewall { "100 tcp/${guest}":
       port   => $guest,
