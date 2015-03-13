5a1acb00a2ed762d79924eba83414bedd0e3d63d
Juan Treminio <jtreminio@gmail.com>
2015-03-13 11:26:48 -0500

    Truthy checkers now use to_bool

---
+++ lib/puppet/parser/functions/array_true.rb
@@ -0,0 +1,34 @@
+dir = File.dirname(File.expand_path(__FILE__))
+require "#{dir}/to_bool.rb"
+
+#
+# array_true.rb
+#
+
+module Puppet::Parser::Functions
+
+  newfunction(:array_true, :type => :rvalue, :doc => <<-'ENDHEREDOC') do |args|
+
+    Returns true if value in array exists and is truthy
+    ENDHEREDOC
+
+    unless args.length == 2
+      raise Puppet::ParseError, ("array_true(): wrong number of arguments (#{args.length}; must be 2)")
+    end
+
+    container = args[0]
+
+    if !container.is_a?(Hash) && !container.is_a?(Array)
+      raise Puppet::ParseError, ("value_true(): first value must be an array, passed #{container.class}")
+    end
+
+    key = args[1]
+
+    if !container.has_key?(key)
+      return false
+    end
+
+    return container[key].to_bool
+
+  end
+end
