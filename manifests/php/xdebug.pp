0697fc5c3e9dacbf547b0f549a58275814a0e9c6
Juan Treminio <jtreminio@gmail.com>
2015-12-05 12:13:01 -0600

    Xdebug uses php-config to get module directory

--- manifests/php/xdebug.pp
+++ manifests/php/xdebug.pp
@@ -28,20 +28,6 @@ class puphpet::php::xdebug (
     }
   } elsif $puphpet::php::settings::enable_xdebug {
     # php 5.6 requires xdebug be compiled, for now
-    case $::operatingsystem {
-      # Debian and Ubuntu slightly differ
-      'debian', 'ubuntu': {
-        if is_dir('/usr/lib/php5/20131226-zts') {
-          $mod_dir = '/usr/lib/php5/20131226-zts'
-        } elsif is_dir('/usr/lib/php5/20131226') {
-          $mod_dir = '/usr/lib/php5/20131226'
-        } else {
-          $mod_dir = '/usr/lib/php/20151012'
-        }
-      }
-      'redhat', 'centos': {$mod_dir = '/usr/lib64/php/modules'}
-    }
-
     vcsrepo { '/.puphpet-stuff/xdebug':
       ensure   => present,
       provider => git,
@@ -52,16 +38,18 @@ class puphpet::php::xdebug (
       creates => '/.puphpet-stuff/xdebug/configure',
       cwd     => '/.puphpet-stuff/xdebug',
     }
-    -> exec { "cp /.puphpet-stuff/xdebug/modules/xdebug.so ${mod_dir}/xdebug.so":
-      creates => "${mod_dir}/xdebug.so",
+    -> exec { 'copy xdebug.so to modules dir':
+      command => "cp /.puphpet-stuff/xdebug/modules/xdebug.so `php-config --extension-dir`/xdebug.so \
+                  && touch /.puphpet-stuff/xdebug-installed",
+      creates => '/.puphpet-stuff/xdebug-installed',
     }
 
     puphpet::php::ini { 'xdebug/zend_extension':
       entry       => "XDEBUG/zend_extension",
-      value       => "${mod_dir}/xdebug.so",
+      value       => 'xdebug.so',
       php_version => $puphpet::php::settings::version,
       webserver   => $webserver,
-      require     => Exec["cp /.puphpet-stuff/xdebug/modules/xdebug.so ${mod_dir}/xdebug.so"],
+      require     => Exec['copy xdebug.so to modules dir'],
     }
   }
 
