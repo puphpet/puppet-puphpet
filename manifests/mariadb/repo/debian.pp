d38340c8de77ff0b9443425d2fc2552b3cf9d7d1
Juan Treminio <jtreminio@gmail.com>
2016-10-04 16:59:40 -0500

    MariaDB has two keys

--- manifests/mariadb/repo/debian.pp
+++ manifests/mariadb/repo/debian.pp
@@ -7,6 +7,12 @@ class puphpet::mariadb::repo::debian (
   $os       = downcase($::operatingsystem)
   $location = "http://mirror.jmu.edu/pub/mariadb/repo/${version}/${os}"
 
+  if ! defined(Apt::Key['199369E5404BD5FC7D2FE43BCBCB082A1BB943DB']) {
+    ::apt::key { '199369E5404BD5FC7D2FE43BCBCB082A1BB943DB':
+      server => 'hkp://keyserver.ubuntu.com:80'
+    }
+  }
+
   ::apt::source { 'mariadb':
     location => $location,
     release  => $::lsbdistcodename,
@@ -15,7 +21,8 @@ class puphpet::mariadb::repo::debian (
       'id'     => '177F4010FE56CA3336300305F1656F24C74CD1D8',
       'server' => 'hkp://keyserver.ubuntu.com:80',
     },
-    include  => { 'src' => true }
+    include  => { 'src' => true },
+    require  => Apt::Key['199369E5404BD5FC7D2FE43BCBCB082A1BB943DB'],
   }
 
   apt::pin { 'mariadb':
