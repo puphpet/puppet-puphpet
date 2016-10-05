791471acfb0fda1fc281d1d5d3c4b612c6ea6388
Juan Treminio <jtreminio@gmail.com>
2016-10-04 22:18:16 -0500

    Removes rpmforge repo

--- manifests/server.pp
+++ manifests/server.pp
@@ -76,9 +76,6 @@ class puphpet::server {
     'redhat': {
       class { 'yum': extrarepo => ['epel'] }
 
-      class { 'yum::repo::rpmforge': }
-      class { 'yum::repo::repoforgeextras': }
-
       Class['::yum'] -> Yum::Managed_yumrepo <| |> -> Package <| |>
 
       if ! defined(Package['augeas']) {
