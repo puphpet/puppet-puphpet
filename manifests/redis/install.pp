f1b9aa023c5b29cc376e1f72d68add1ac4de8e02
Juan Treminio <jtreminio@gmail.com>
2018-01-20 22:13:01 -0600

    Revert "Redis module switched to eyp/redis"
    This reverts commit ff5087f094c7851e045a722e689cb4c2cdfd25b0.

--- manifests/redis/install.pp
+++ manifests/redis/install.pp
@@ -12,6 +12,12 @@ class puphpet::redis::install {
   $nginx  = $puphpet::params::hiera['nginx']
   $php    = $puphpet::params::hiera['php']
 
+  if $::operatingsystem == 'ubuntu' and $::lsbdistcodename == 'trusty' {
+    $manage_repo = true
+  } else {
+    $manage_repo = false
+  }
+
   if array_true($apache, 'install') or array_true($nginx, 'install') {
     $webserver_restart = true
   } else {
@@ -25,48 +31,36 @@ class puphpet::redis::install {
   }
 
   $settings = delete(deep_merge({
-    'manage_package' => true,
-  }, $redis['settings']), ['conf_port', 'port', 'manage_repo'])
+    'port'        => $port,
+    'manage_repo' => $manage_repo,
+  }, $redis['settings']), 'conf_port')
 
-  if $::operatingsystem == 'ubuntu' {
-    if ! defined(Apt::Key['136221EE520DDFAF0A905689B9316A7BC7917B12']) {
-      ::apt::key { '136221EE520DDFAF0A905689B9316A7BC7917B12':
-        server => 'hkp://keyserver.ubuntu.com:80'
-      }
-    }
+  $config_owner = $redis::params::config_owner
+  $config_group = $redis::params::config_group
 
-    if ! defined(Apt::Ppa['ppa:chris-lea/redis-server']) {
-      ::apt::ppa { 'ppa:chris-lea/redis-server':
-        require => ::Apt::Key['136221EE520DDFAF0A905689B9316A7BC7917B12']
-      }
-    }
-  }
-
-  if ! defined(Group[$redis::params::default_redis_group]) {
-    group { $redis::params::default_redis_group:
-      ensure => present,
-      before => Class['redis']
+  # Non-Debian install does not create this directory by default
+  if ! defined(File['/var/run/redis']) and $::osfamily != 'Debian' {
+    file { '/var/run/redis':
+      ensure  => 'directory',
+      owner   => $config_owner,
+      group   => $config_group,
+      require => Class['redis'],
     }
   }
 
-  if ! defined(User[$redis::params::default_redis_user]) {
-    user { $redis::params::default_redis_user:
-      ensure     => present,
-      managehome => false,
-      groups     => [$redis::params::default_redis_group],
-      before     => Class['redis']
-    }
-  }
+  $tmpfiles_conf = '/usr/lib/tmpfiles.d/redis.conf'
 
-  if ! defined(Puphpet::Firewall::Port["${$port}"]) {
-    puphpet::firewall::port { "${$port}":
-      before => Class['redis'],
+  if ! defined(File[$tmpfiles_conf]) {
+    file { $tmpfiles_conf:
+      ensure  => 'present',
+      content => "d /var/run/redis 0755 ${config_owner} ${config_group} -",
+      owner   => $config_owner,
+      group   => 'root',
+      require => Class['redis'],
     }
   }
 
   create_resources('class', { 'redis' => $settings })
-
-  redis::instance { $port: }
 
   if array_true($php, 'install') and ! defined(Puphpet::Php::Module::Pecl['redis']) {
     puphpet::php::module::pecl { 'redis':
