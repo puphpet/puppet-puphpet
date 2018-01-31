9664bd901784590c040b8807a3f7efbd98bb6c0a
Juan Treminio <jtreminio@gmail.com>
2018-01-30 19:51:16 -0600

    Allows Ubuntu 16.04 to install MySQL 5.6; fixes puphpet/puphpet#2648

--- manifests/mysql/repo/debian.pp
+++ manifests/mysql/repo/debian.pp
@@ -4,45 +4,79 @@ class puphpet::mysql::repo::debian (
 
   $os = downcase($::operatingsystem)
 
-  if $version in ['56', '5.6'] {
-    ::apt::pin { 'repo.mysql.com-apt':
-      priority   => 1000,
-      originator => 'repo.mysql.com-apt',
+  if $version in ['56', '5.6', 56, 5.6] {
+    $repo_version = '5.6'
+    $dist         = $::lsbdistcodename ?{
+      'xenial' => 'wily', # MySQL 5.6 not available for Xenial, have to use Wily
+      default  => $::lsbdistcodename,
     }
+  } else {
+    $repo_version = '5.7'
+    $dist         = $::lsbdistcodename
+  }
 
-    if ! defined(Apt::Source['repo.mysql.com-apt']) {
-      ::apt::source { 'repo.mysql.com-apt':
-        location => "http://repo.mysql.com/apt/${os}",
-        release  => $::lsbdistcodename,
-        repos    => 'mysql-5.6',
-        include  => { 'src' => true },
-        require  => Apt::Pin['repo.mysql.com-apt'],
-        before   => [
-          Class['mysql::client'],
-          Class['mysql::server'],
-        ],
-      }
+  if ! defined(Apt::Key['A4A9406876FCBD3C456770C88C718D3B5072E1F5']) {
+    apt::key { 'A4A9406876FCBD3C456770C88C718D3B5072E1F5':
+      server => 'hkp://keyserver.ubuntu.com:80'
     }
   }
 
-  if $version in ['57', '5.7'] {
-    ::apt::pin { 'repo.mysql.com-apt':
-      priority   => 1000,
-      originator => 'repo.mysql.com-apt',
-    }
+  apt::pin { 'mysql-pin':
+    priority        => 1002,
+    version         => "${repo_version}.*",
+    packages        => [
+      'mysql-client',
+      'mysql-common',
+      'mysql-server',
+      'mysql-testsuite',
+    ],
+  }
 
-    if ! defined(Apt::Source['repo.mysql.com-apt']) {
-      apt::source { 'repo.mysql.com-apt':
-        location => "http://repo.mysql.com/apt/${os}",
-        release  => $::lsbdistcodename,
-        repos    => 'mysql-5.7',
-        include  => { 'src' => true },
-        require  => Apt::Pin['repo.mysql.com-apt'],
-        before   => [
-          Class['mysql::client'],
-          Class['mysql::server'],
-        ],
-      }
+  if ! defined(Apt::Source['mysql-apt-config']) {
+    ::apt::source { 'repo.mysql.com-apt':
+      location => 'http://repo.mysql.com/apt/ubuntu',
+      release  => $dist,
+      repos    => "mysql-${repo_version}",
+      require  => [
+        Apt::Pin['mysql-pin'],
+        Apt::Key['A4A9406876FCBD3C456770C88C718D3B5072E1F5']
+      ],
+      before   => [
+        Class['mysql::client'],
+        Class['mysql::server'],
+      ],
+    }
+  }
+
+  if ! defined(Apt::Source['repo.mysql.com-apt']) {
+    ::apt::source { 'repo.mysql.com-apt':
+      location => 'http://repo.mysql.com/apt/ubuntu',
+      release  => $dist,
+      repos    => "mysql-${repo_version}",
+      require  => [
+        Apt::Pin['mysql-pin'],
+        Apt::Key['A4A9406876FCBD3C456770C88C718D3B5072E1F5']
+      ],
+      before   => [
+        Class['mysql::client'],
+        Class['mysql::server'],
+      ],
+    }
+  }
+
+  if ! defined(Apt::Source['repo.mysql.com-apt']) {
+    ::apt::source { 'repo.mysql.com-apt':
+      location => 'http://repo.mysql.com/apt/ubuntu',
+      release  => $dist,
+      repos    => "mysql-${repo_version}",
+      require  => [
+        Apt::Pin['mysql-pin'],
+        Apt::Key['A4A9406876FCBD3C456770C88C718D3B5072E1F5']
+      ],
+      before   => [
+        Class['mysql::client'],
+        Class['mysql::server'],
+      ],
     }
   }
 
